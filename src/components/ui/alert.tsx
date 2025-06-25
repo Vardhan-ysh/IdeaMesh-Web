2d0d2b8a05a1c08e4828b99321bdd503da38b4f0
Yashvardhan Singh <yashvardhansingh112233@gmail.com>
2025-06-25 20:28:26 +0000

    refactor(ui): Refine glassmorphism in alert component

--- src/components/ui/alert.tsx
+++ src/components/ui/alert.tsx
@@ -1,16 +1,18 @@
+"use client"
+
 import * as React from "react"
 import { cva, type VariantProps } from "class-variance-authority"
 
 import { cn } from "@/lib/utils"
 
 const alertVariants = cva(
-  "relative w-full rounded-lg border p-4 [&>svg~*]:pl-7 [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-foreground",
+  "relative w-full rounded-lg border p-4 backdrop-blur-lg [&>svg~*]:pl-7 [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-foreground",
   {
     variants: {
       variant: {
-        default: "bg-background text-foreground",
+        default: "border-border/20 bg-background/80 text-foreground",
         destructive:
-          "border-destructive/50 text-destructive dark:border-destructive [&>svg]:text-destructive",
+          "border-destructive/50 bg-destructive/80 text-destructive dark:border-destructive [&>svg]:text-destructive",
       },
     },
     defaultVariants: {
